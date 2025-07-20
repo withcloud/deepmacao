2ba006a913f586c68ecfae7f50e52698e62f59fc
Comus Leong <comus.leong@gmail.com>
2025-07-20 15:40:38 +0800

    更新 title, description

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ import './globals.css';
 import { SessionProvider } from 'next-auth/react';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://chat.vercel.ai'),
-  title: 'Next.js Chatbot Template',
-  description: 'Next.js chatbot template using the AI SDK.',
+  metadataBase: new URL('https://deepmacao.com'),
+  title: 'DeepMacao',
+  description: 'DeepMacao is a chatbot that can help you with your questions.',
 };
 
 export const viewport = {
